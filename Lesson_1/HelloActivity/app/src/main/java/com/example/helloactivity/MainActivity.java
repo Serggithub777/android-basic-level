95a88117553baa121a29adc09a74c2149ee1104c
SergSerg <sergio.ne@inbox.ru>
2020-04-08 16:16:03 +0600

    Change Main_Activity

--- Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
+++ Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.helloactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText enterText;
+    private TextView textOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        enterText = findViewById(R.id.editTextEnter);
+        textOut = findViewById(R.id.textViewOut);
+
+        Button buttonGo = findViewById(R.id.buttonGo);
+        buttonGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String str = enterText.getText().toString();
+                textOut.setText(str);
+            }
+        });
 
     }
 }
