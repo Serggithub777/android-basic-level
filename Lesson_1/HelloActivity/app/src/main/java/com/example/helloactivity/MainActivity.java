726c7300246c2097baae8e9471ff6ea09c29f05b
SergSerg <sergio.ne@inbox.ru>
2020-04-08 16:34:49 +0600

    Add treatment for ImageButton

--- Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
+++ Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
@@ -7,12 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText enterText;
     private TextView textOut;
-
+    View.OnClickListener listener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,16 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         enterText = findViewById(R.id.editTextEnter);
         textOut = findViewById(R.id.textViewOut);
-
+        ImageButton imageButton = findViewById(R.id.imageButton);
         Button buttonGo = findViewById(R.id.buttonGo);
-        buttonGo.setOnClickListener(new View.OnClickListener() {
+
+        buttonGo.setOnClickListener(listener);
+
+        listener = new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.buttonGo:
+                        String str = enterText.getText().toString();
+                        textOut.setText(str);
+                        break;
+                    case R.id.imageButton:
+                        textOut.setText("Push Image Button!");
+                }
 
-                String str = enterText.getText().toString();
-                textOut.setText(str);
             }
-        });
+        };
 
     }
 }
