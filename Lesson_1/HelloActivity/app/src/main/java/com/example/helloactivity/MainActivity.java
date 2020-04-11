64b87cb099608e26a0969fd262d654cf83a52992
SergSerg <sergio.ne@inbox.ru>
2020-04-11 20:20:19 +0600

    Lesson_2 added

--- Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
+++ Lesson_1/HelloActivity/app/src/main/java/com/example/helloactivity/MainActivity.java
@@ -2,19 +2,16 @@ package com.example.helloactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText enterText;
     private TextView textOut;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         enterText = findViewById(R.id.editTextEnter);
         textOut = findViewById(R.id.textViewOut);
-        ImageButton imageButton = findViewById(R.id.imageButton);
-        Button buttonGo = findViewById(R.id.buttonGo);
 
-        View.OnClickListener listener =new View.OnClickListener() {
+        Button buttonGo = findViewById(R.id.buttonGo);
+        buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.buttonGo:
-                        Toast.makeText(MainActivity.this,"Push Button GO!",Toast.LENGTH_SHORT).show();
-                        String str = enterText.getText().toString();
-                        textOut.setText(str);
-                        break;
-                    case R.id.imageButton:
-                        Toast.makeText(MainActivity.this,"Push Image Button!",Toast.LENGTH_SHORT).show();
-                        String text = "Push Image Button!!";
-                        textOut.setText(text);
-                        break;
-                }
 
+                String str = enterText.getText().toString();
+                textOut.setText(str);
             }
-        };
-        buttonGo.setOnClickListener(listener);
-        imageButton.setOnClickListener(listener);
+        });
 
     }
 }
