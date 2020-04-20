143fbdb3206bbb0359b976e9a9e1f83de3423f8f
SergSerg <sergio.ne@inbox.ru>
2020-04-20 17:42:22 +0600

    Создаем интерфейс Constants d который помещаем все константы затем при имплементации интерфейса в классы используем константы. Вопрос? Как это влияет на инкапсуляцию обычно все константы private static final а интерфейс public.

--- Lesson_4/SecondActivity/app/src/main/java/com/example/secondactivity/Constants.java
+++ Lesson_4/SecondActivity/app/src/main/java/com/example/secondactivity/Constants.java
@@ -1,5 +1,5 @@
 package com.example.secondactivity;
 
 public interface Constants {
-    String KEY_TEXT = "com.example.secondactivity.TEXT";//в ключ добавлен путь к нему
+    String KEY_TEXT = "com.example.secondactivity.TEXT";//в ключ добавлен путь к нему в папке
 }
