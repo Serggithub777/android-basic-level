1b2efebcbd5a2d3a2471779d7331391e3656b265
SergSerg <sergio.ne@inbox.ru>
2020-04-25 23:44:05 +0600

    Добавление первого фрагмента при создании MainActivity d портретной ориентации

--- Lesson_5/CreateFragmentCat/app/src/main/java/com/example/createfragmentcat/MainActivity.java
+++ Lesson_5/CreateFragmentCat/app/src/main/java/com/example/createfragmentcat/MainActivity.java
@@ -3,16 +3,33 @@ package com.example.createfragmentcat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements Fragment1.OnSelectedButtonListener {
-
+    boolean mIsDynamic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //получаем ссылку на второй фрагмент по ID
+        Fragment2 fragment2 = (Fragment2) fragmentManager.findFragmentById(R.id.fragment2);
+        mIsDynamic = fragment2 == null || !fragment2.isInLayout();
+        Toast.makeText(getApplicationContext(), mIsDynamic+" ", Toast.LENGTH_SHORT).show();
+
+        if (mIsDynamic) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            //создаем и добавляем новый фрагмент
+            Fragment1 fragment1 = new Fragment1();
+            ft.add(R.id.container, fragment1,"fragment1");
+            //подтверждаем транзакцию
+            ft.commit();
+
+        }
     }
 
     @Override
@@ -20,19 +37,27 @@ public class MainActivity extends AppCompatActivity implements Fragment1.OnSelec
         //подключаем FragmentManager
         FragmentManager fragmentManager = getSupportFragmentManager();
         //получаем ссылку на второй фрагмент по ID
-        Fragment2 fragment2 = (Fragment2) fragmentManager.findFragmentById(R.id.fragment2);
+        Fragment2 fragment2 ;
         /*//выводим нужную информацию
         if (fragment2 != null) {
             fragment2.setDescription(buttonIndex);
         }*/
         //если фрагмента не существует или он невидим
-        if (fragment2 == null || !fragment2.isVisible()) {
+        if (mIsDynamic) {
             //запускаем вторую активность
-            Intent intent = new Intent(getApplicationContext(), Activity_second.class);
+            /*Intent intent = new Intent(getApplicationContext(), Activity_second.class);
             intent.putExtra("buttonIndex", buttonIndex);
-            startActivity(intent);
+            startActivity(intent);*/
+
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            fragment2 = new Fragment2();
+            ft.replace(R.id.container, fragment2, "fragment2");
+            ft.addToBackStack(null);
+            ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+            ft.commit();
         } else {
             //выводим информацию во втором фрагменте
+            fragment2 = (Fragment2) fragmentManager.findFragmentById(R.id.fragment2);
             fragment2.setDescription(buttonIndex);
         }
     }
--- Lesson_5/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ Lesson_5/FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,31 +1,47 @@
 package com.example.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements FirstFragment.onSelectedButtonListener {
-
+    private boolean mIsDynamic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        SecondFragment secondFragment = (SecondFragment) fm.findFragmentById(R.id.fragmentSecond);
+        mIsDynamic = secondFragment == null || !secondFragment.isInLayout();
+        if (mIsDynamic) {
+            FragmentTransaction ft = fm.beginTransaction();
+            FirstFragment firstFragment = new FirstFragment();
+            ft.add(R.id.container, firstFragment, "first_fragment");
+            ft.commit();
+
+        }
+
     }
 
     @Override //реализуем метод интерфейса
     public void onButtonSelected(String bodySms) {
         //вызываем менеджер фрагментов
         FragmentManager fm = getSupportFragmentManager();
+        SecondFragment secondFragment;
+
         //получаем ссылку на второй фрагмент
-        SecondFragment secondFragment = (SecondFragment) fm.findFragmentById(R.id.fragmentSecond);
+        //SecondFragment secondFragment = (SecondFragment) fm.findFragmentById(R.id.fragmentSecond);
         //если второй фрагмент не создан или не видим создаем и запускаем активность
-        if (secondFragment == null || !secondFragment.isVisible()) {
-           Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        if (mIsDynamic) {
+           /*Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
             intent.putExtra("SMS", bodySms);
-            startActivity(intent);
+            startActivity(intent);*/
         }else{
+            secondFragment = (SecondFragment) fm.findFragmentById(R.id.fragmentSecond);
             secondFragment.setTextSmsBody(bodySms);
         }
 
