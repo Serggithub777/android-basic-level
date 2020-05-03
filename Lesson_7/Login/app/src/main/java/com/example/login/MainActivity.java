fff8dd7b7826173b88cca48cd5a913fd14081409
SergSerg <sergio.ne@inbox.ru>
2020-05-03 21:21:46 +0600

    Lesson_7 Материальный дизйн Login и Password

--- Lesson_7/Login/app/src/main/java/com/example/login/MainActivity.java
+++ Lesson_7/Login/app/src/main/java/com/example/login/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.login;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    private TextInputEditText loginInputEditText;
+    private TextInputEditText passwordInputEditText;
+    private TextInputLayout loginLayout;
+    private TextInputLayout passwordLayout;
 
+    // Регулярные выражения позволяют проверить на соответствие шаблону
+    // Это имя. Первая буква большая латинская, остальные маленькие латинские
+    Pattern checkLogin = Pattern. compile("^[A-Z][a-z]{2,}$");
+    // Это пароль, минимум 6 символов, обязательны маленькая буква, большая буква, цифра
+    Pattern checkPassword = Pattern. compile("^(?=^.{6,}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginInputEditText = findViewById(R.id.inputLoginName);
+        passwordInputEditText = findViewById(R.id.inputPassword);
+        loginLayout = findViewById(R.id.loginNameLayout);
+        passwordLayout = findViewById(R.id.passwordInputLayout);
+
+        //устанавливаем фокус слушатель на логин и реализуем метод onFocusChange с проверкой паттерна и отменой ошибки
+        loginInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    return;
+                    TextView tv = (TextView) v;
+                validate(tv, checkLogin, loginLayout, "It's not Name!");
+            }
+            });
+        //устанавливаем фокус слушатель на пароль и реализуем метод onFocusChange с проверкой паттерна и отменой ошибки
+
+        passwordInputEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) return;
+                TextView tv = (TextView) v;
+                validate(tv, checkPassword, passwordLayout, "You entered SIMPLE password!");
+           }
+        });
+    }
+
+    private void validate(TextView tv, Pattern pattern, TextInputLayout textInputLayout, String message) {
+        String value = tv.getText().toString();
+        if (pattern.matcher(value).matches()) {
+            //tv.setError(null);
+            showError(textInputLayout, null);
+        } else {
+            showError(textInputLayout, message);
+            //tv.setError(message);
+        }
+    }
+
+    private void showError(TextInputLayout textInputLayout, String message) {
+        textInputLayout.setError(message);
     }
 }
